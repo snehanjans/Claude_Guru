47ef7bb61101b88be0880be4051e6b1af7fe2468
sachinsinha0 <sachin.sinha@greatlearning.in>
2026-03-16 16:36:39 +0530

    Merge branch 'course-v1'

--- src/components/dialogs/index.tsx
+++ src/components/dialogs/index.tsx
@@ -10,7 +10,7 @@ import { LearnerRatingsDialog } from "./LearnerRatingsDialog";
 import { PollBuilderDialog } from "./PollBuilderDialog";
 import { MarkUnavailableModal } from "./MarkUnavailableModal";
 import { AddAvailabilityModal } from "./AddAvailabilityModal";
-import { SessionDetailsModal } from "./SessionDetailsModal";
+// import { SessionDetailsModal } from "./SessionDetailsModal";
 import { CourseDetailDialog } from "./CourseDetailDialog";
 
 /**
@@ -32,7 +32,7 @@ export function GlobalDialogs() {
       <PollBuilderDialog />
       <MarkUnavailableModal />
       <AddAvailabilityModal />
-      <SessionDetailsModal />
+      {/* <SessionDetailsModal /> */}
       <CourseDetailDialog />
     </>
   );
